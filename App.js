55a373811a757b363541dff272d9a28a1b10347c
Akhilesh Pandey <akhilesh.flairlabs@ext.idfy.com>
2024-08-12 23:16:38 +0530

    first episode completed of namaste react

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading1 = React.createElement(
+  "h1",
+  { id: "heading1" },
+  "I am an H1 tag"
+);
+const heading2 = React.createElement(
+  "h2",
+  { id: "heading2" },
+  "I am an H2 tag"
+);
+const child1 = React.createElement("div", { id: "child1" },[heading1,heading2]);
+const child2 = React.createElement("div", { id: "child2" }, [
+  heading1,
+  heading2,
+]);
+
+const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
